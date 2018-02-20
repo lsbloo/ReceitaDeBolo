509263cbe9d5c3c4a3c1e360211cce42421024b8
osvaldoAiron <osvaldo_cavalcanti_filho@hotmail.com>
2018-02-20 20:37:43 -0300

    ssh2

--- scriptSsh/arq.sh
+++ scriptSsh/arq.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 function verificarPorts(){
+
 	 # Verificar quais portas estao abertas.
 	verifica=$( which nmap )
 	if [ -z $verifica ]; then
@@ -11,34 +12,47 @@ function verificarPorts(){
 	fi
         grep -i "80/tcp" portas.txt > http.txt
 	http=$(head -n 2 http.txt | tail -n 1 | sed 's/ //g')
-	echo $http
+	#echo $http
 	grep -i "22/tcp" portas.txt > ssh.txt
 	ssh=$(head -n 2 ssh.txt | tail -n 1 | sed 's/ //g')
-	echo $ssh
+	#echo $ssh
 	if [ -z $http ]; then
 		echo 'http fechado'
-		if [ -z $ssh ]; then
-			echo 'ssh fechado'
-		else
-		
-			ifconfig > ip.txt
-		fi
-
 	else
 		ifconfig > ip.txt
 	fi
 
+	if [ -z $ssh ]; then
+		echo 'ssh fechado'
+		sudo apt-get install ssh
+		
+	else
+		arv_ssh=$(which ssh)
+		echo $arv_ss
+		
+		ifconfig > ip.txt
+	fi
+
+
 	
 }
+
 function resetPass(){
 	 #resetar passwd
 	usuario=($USER)
 	touch s.txt
 	grep -i $usuario /etc/passwd > s.txt
 	password=$(head -1 s.txt)
-	echo $password
+	if [ -z $usuario ]; then
+		echo 'not found'
+	else
+		# remover a senha do usuario X
+		sed -i 's/^'$usuario':*/'$usuario'/' /etc/passwd
+	fi
+	#echo $password
 	
 
-}	
+}
+ 	
 verificarPorts
 resetPass
